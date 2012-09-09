83d50bc8f61e1ed3111781e05c506f2acb5a217d
mitayun@gmail.com <mitayun@gmail.com@03e0ff34-84b9-e226-025c-5aa6bb2b26ab>
2012-09-09 08:36:39 +0000

    flash working. TODO: clean up + config files

---
+++ trunk/NarwalQuad_F407/User/quad_lib/config_default.h
@@ -0,0 +1,18 @@
+/*
+ * config_default.h
+ *
+ *  Created on: Sep 9, 2012
+ *      Author: GrubyGrub
+ */
+#include "stm32f4xx.h"
+
+#ifndef CONFIG_DEFAULT_H_
+#define CONFIG_DEFAULT_H_
+
+#define DEFAULT_CONFIG_VERSION      100
+
+#define DEFAULT_IMU_ACC_BIAS_X      -1.65
+#define DEFAULT_IMU_ACC_BIAS_Y      -1.65
+#define DEFAULT_IMU_ACC_BIAS_Z      -1.65
+
+#endif /* CONFIG_DEFAULT_H_ */
